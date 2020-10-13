57580ac462446605d46b14fe4c13b735ebdfe74f
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-10-13 12:43:08 +0300

    start shared transition tutorials

---
+++ Tutorial3-1Transitions/src/main/java/com/smarttoolfactory/tutorial3_1transitions/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.smarttoolfactory.tutorial3_1transitions
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.smarttoolfactory.tutorial3_1transitions.adapter.BaseAdapter
+import com.smarttoolfactory.tutorial3_1transitions.adapter.ChapterSelectionAdapter
+import com.smarttoolfactory.tutorial3_1transitions.adapter.model.ActivityClassModel
+import com.smarttoolfactory.tutorial3_1transitions.databinding.ActivityMainBinding
+import java.util.ArrayList
+
+class MainActivity : AppCompatActivity(), BaseAdapter.OnRecyclerViewItemClickListener {
+
+    private val activityClassModels = ArrayList<ActivityClassModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        title = "MainActivity"
+
+        val activityMainBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        addChapters()
+
+        val recyclerView = activityMainBinding.recyclerView
+
+        recyclerView.apply {
+
+            // Use fixed size for performance
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = LinearLayoutManager(this@MainActivity)
+
+            // Add vertical divider
+            addItemDecoration(
+                DividerItemDecoration(
+                    this@MainActivity,
+                    DividerItemDecoration.VERTICAL
+                )
+            )
+
+            // Add Adapter
+            recyclerView.adapter = ChapterSelectionAdapter(activityClassModels).apply {
+                setOnItemClickListener(this@MainActivity)
+            }
+        }
+    }
+
+    private fun addChapters() {
+
+        // Add Activities to list to be displayed on RecyclerView
+//        activityClassModels.add(
+//            ActivityClassModel(
+//                Activity1_1Basics::class.java,
+//                getString(R.string.activity1_1)
+//            )
+//        )
+
+
+    }
+
+    @Override
+    override fun onItemClicked(view: View, position: Int) {
+        Intent(this@MainActivity, activityClassModels[position].clazz).also {
+            startActivity(it)
+        }
+    }
+}
---
+++ Tutorial3-1Transitions/src/main/java/com/smarttoolfactory/tutorial3_1transitions/adapter/model/ActivityClassModel.kt
@@ -0,0 +1,3 @@
+package com.smarttoolfactory.tutorial3_1transitions.adapter.model
+
+data class ActivityClassModel(val clazz: Class<*>, val description: String = clazz.name)
